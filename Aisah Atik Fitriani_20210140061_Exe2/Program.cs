438da65fe4c25c35429c52e31314ff0e80adf2ed
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-02 16:52:00 +0700

    Add Public Void Merge Sort

--- Aisah Atik Fitriani_20210140061_Exe2/Program.cs
+++ Aisah Atik Fitriani_20210140061_Exe2/Program.cs
@@ -77,5 +77,89 @@ namespace Exercise2
             }
             
         }
-    }
+
+        public void MergeSort()
+        {
+            int high, mid, low;
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] aisah = new int[81];
+            int AF, num, pos ;
+
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                aisah[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Input int array : ");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(aisah[k] + " ");
+                Console.Write("\n");
+            }
+
+            if (low > high)
+            {
+                mid = (low + high) / 2;
+                
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("================");
+                    Console.WriteLine("1. Linear Search");
+                    Console.WriteLine("2. Binary Search");
+                    Console.WriteLine("3. Exit");
+                    Console.Write("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine("Linear Search");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.InsertionSort();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.MergeSort();
+                            break;
+                        case 3:
+                            Console.WriteLine("exit.");
+                            break;
+                        default:
+                            Console.WriteLine("Error");
+                            break;
+
+                    }
+                    Console.WriteLine("\n Pilih Menu Lagi ?????? (y/n) :");
+                    ch = char.Parse(Console.ReadLine().ToLower());
+                    Console.Clear();
+                }
+                while (ch == 'y');
+
+                //to exit from the console
+                Console.WriteLine("\n Press Return to exit.");
+                Console.ReadLine();
+
+            }
+            while (pilihanmenu != 3);
+        }
 }
