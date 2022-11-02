8077aa4c2394ee7b4988997cb85dadbd5f6f9f3b
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-02 15:55:26 +0700

    Add Public Void Input & While Condition

--- Aisah Atik Fitriani_20210140061_Exe2/Program.cs
+++ Aisah Atik Fitriani_20210140061_Exe2/Program.cs
@@ -13,6 +13,17 @@ namespace Exercise2
 
         public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 81))
+                    break;
+                else
+                    Console.WriteLine("\n.Array should have minimum 1 and maximum 81 elements.\n");
+            }
+
 
         }
     }
