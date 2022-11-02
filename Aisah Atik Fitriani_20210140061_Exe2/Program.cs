73f4c8c6fc9f323909b0b96b501567a2a34d7878
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-02 15:48:42 +0700

    Add namespace Exercise 2

--- Aisah Atik Fitriani_20210140061_Exe2/Program.cs
+++ Aisah Atik Fitriani_20210140061_Exe2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise2
+{
+    class Program
+    {
+        int[] aisah = new int[81];
+        int n;
+        int i;
+
+        public void input()
+        {
+
+        }
+    }
+}
